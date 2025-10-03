51d44e62e21bb381e6f27c8be573a2412992643b
DylanH912 <144183652+DylanH912@users.noreply.github.com>
2025-10-03 15:05:33 -0500

    Add files via upload

---
+++ QueueInterface.java
@@ -0,0 +1,7 @@
+public interface QueueInterface<T> {
+    public void enqueue(T item);
+    public T dequeue();
+    public boolean isFull();
+    public boolean isEmpty();
+    public Integer length();
+}

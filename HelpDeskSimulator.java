f9ea34f3d9d920f34adf32f00aab3d308384c447
SuttonZimmerman <szimmerm@carrollu.edu>
2025-10-06 09:02:05 -0500

    Mistook HelpDeskSimulator as helpdesk moved code over. I have a question on how we plan on doing this because I believe instead of a student array we should do a ArrayBoundedQueue<Student>

--- HelpDeskSimulator.java
+++ HelpDeskSimulator.java
@@ -1,12 +1,46 @@
 public class HelpDeskSimulator{ //Keeps the log
-    private static int step;
+    private int time;
 
-    private static ArrayBoundedQueue<Student> levelOne = new ArrayBoundedQueue<Student>;
-    private static ArrayBoundedQueue<Student> levelTwo = new ArrayBoundedQueue<Student>;
-    private static ArrayBoundedQueue<Student> levelThree = new ArrayBoundedQueue<Student>;
-    private static ArrayBoundedQueue<Student> levelFour = new ArrayBoundedQueue<Student>;
+    private  ArrayBoundedQueue<Student> levelOne;
+    private  ArrayBoundedQueue<Student> levelTwo;
+    private  ArrayBoundedQueue<Student> levelThree;
+    private  ArrayBoundedQueue<Student> levelFour;
 
-    private static ArrayBoundedQueue<Student>[] waitingArea = [levelOne, levelTwo, levelThree, levelFour];
+    //private ArrayBoundedQueue<Student>[] waitingArea;
+
+    //------ No-Constraints-Constructor--------
+    public HelpDeskSimulator(){
+        this.time = 0;
+
+        this.levelOne = new ArrayBoundedQueue<Student>;
+        this.levelTwo = new ArrayBoundedQueue<Student>;
+        this.levelThree = new ArrayBoundedQueue<Student>;
+        this.levelFour = new ArrayBoundedQueue<Student>;
+
+        //ArrayBoundedQueue<Student>[] waitingArea = [levelOne, levelTwo, levelThree, levelFour];
+    }
+
+    //-------------- addStudent ----------------
+    public void addStudent(String name, int course, int workload){
+        Student newStudent = new Student(name, course, workload);
+
+        if (course <= 199 &&  levelOne.length < 3){
+            levelOne.enqueue(newStudent)
+        } else if (course <= 299 && levelTwo.length < 3){
+            levelTwo.enqueue(newStudent)
+        } else if (course <= 399 && levelThree.length < 3){
+            levelThree.enqueue(newStudent)
+        } else if (course <= 499 && levelFour.length < 3){
+            levelFour.enqueue(newStudent)
+        } else {
+            System.out.print("****Temp Turned Away " + newStudent.getName() + "*****")
+        }
+    }
+
+    public int getTime(){
+        return this.time;
+    }
+
 
 
 }

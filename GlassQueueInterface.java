bcb407b64530b9b95d6cd0da896a1aaf1f2fa7e4
SuttonZimmerman <szimmerm@carrollu.edu>
2025-10-06 09:17:08 -0500

    Adjusted ArrayBoundedQueue to have an adjustable max, Created toString()

---
+++ GlassQueueInterface.java
@@ -0,0 +1,4 @@
+public interface GlassQueueInterface<T> extends QueueInterface<T> {
+    public T peekFront();
+    public T peekRear();
+}
--- HelpDesk.java
+++ HelpDesk.java
@@ -2,18 +2,18 @@ public class HelpDesk{
     private int time; // current simulation time in minutes
     private LLNode<Student> currentStudent; // the student currently being helped
     private String status; // current status of the help desk (e.g., "IDLE" or "Helping [Student Name] from [Course Number]")
-    private Student[] queue100 = new Student[3];
-    private Student[] queue200 = new Student[3];
-    private Student [] queue300 = new Student[3];
-    private Student[] queue400 = new Student[3];
+    private ArrayBoundedQueue<Student>[] queue100 = new ArrayBoundedQueue<Student>[3];                //I believe these need to be a ArrayBoundedQueues ------------------- SZ
+    private ArrayBoundedQueue<Student>[] queue200 = new ArrayBoundedQueue<Student>[3];
+    private ArrayBoundedQueue<Student> [] queue300 = new ArrayBoundedQueue<Student>[3];
+    private ArrayBoundedQueue<Student>[] queue400 = new ArrayBoundedQueue<Student>[3];
 
-public void step(){
 /*• Advance the simulation one minute.
 • This method handles the time-stepped simulation logic.
 • Checks if the current student being helped is finished.
 • If no student is being helped, retrieves the next student from the highest-priority non-empty
 queue.
 */
+public void step(){
     if(currentStudent = null){
         this.status = "IDLE";
     }
@@ -23,7 +23,7 @@ queue.
     this.time++;
 
 }
-public void addStudent(String name, int course, int workload){
+
 /*• Add an arriving student with the indicated name, course number, and workload (minutes
 needed).
 • Determines the appropriate priority queue based on course number (course/100 * 100).
@@ -31,24 +31,50 @@ needed).
 • If that queue is full, attempts escalation to higher-numbered course queues.
 • If all escalation attempts fail, logs that the student was turned away.
 */
+public void addStudent(String name, int course, int workload){
+    Student newStudent = new Student(name, course, workload);
 
+        if (course <= 199 &&  !queue100.isFull()){
+            queue100.enqueue(newStudent)
+        } else if (course <= 299 && !queue200.isFull()){
+            queue200.enqueue(newStudent)
+        } else if (course <= 399 && !queue300.isFull()){
+            queue300.enqueue(newStudent)
+        } else if (course <= 499 && !queue400.isFull()){
+            queue400.enqueue(newStudent)
+        } else {
+            System.out.print("****Temp Turned Away " + newStudent.getName() + "*****")
+        }
 }
+
+/*Get the current simulation time in minutes.
+  Returns the current value of the simulation clock.
+*/
 public int getTime(){
-/*• Get the current simulation time in minutes.
-• Returns the current value of the simulation clock.
-*/
-return this.time;
 
+return this.time;
 }
+
+/*Return the status of the simulation at the current time.
+  Example: "Time 2, Helping Jack from CSC110" or "Time 0, IDLE"
+*/
 public String toString(){
-/*• Return the status of the simulation at the current time.
-• Example: "Time 2, Helping Jack from CSC110" or "Time 0, IDLE"
-*/
+    name = currentStudent.getName()
+    course = currentStudent.getCourse()
+    
+    if (status == "IDLE"){
+        System.out.printf("Time %d, IDLE", this.time);
+    }
+    else{
+        System.out.printf("Time %d, Helping %s from %s", this.time, name, course);
+    }
 }
-public String getLog(){
-/*• Return the entire HelpDesk session log from beginning to end.
-• Contains a chronological record of all queuing, helping, and completion events.
+
+/*Return the entire HelpDesk session log from beginning to end.
+ Contains a chronological record of all queuing, helping, and completion events.
 */
+public String getLog(){
+
     return " " + this.time + ", " + this.status;
 }
 
